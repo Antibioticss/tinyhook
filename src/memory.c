254ff110ab599388cf31482a041501cd8d575167
Antibiotics <Antibioticss@users.noreply.github.com>
2026-03-15 21:24:20 +0800

    fixed write_mem calling mach_vm_protect_trap on ios

--- src/memory.c
+++ src/memory.c
@@ -6,21 +6,25 @@
     #include <mach/mach_error.h> // mach_error_string()
 #endif
 
+#if TARGET_OS_OSX
+    #define mach_vm_protect mach_vm_protect_trap
+
 __attribute__((naked)) static kern_return_t mach_vm_protect_trap(mach_port_name_t task, mach_vm_address_t address,
                                                                  mach_vm_size_t size, boolean_t set_maximum,
                                                                  vm_prot_t new_protection) {
-#ifdef __aarch64__
+    #ifdef __aarch64__
     asm volatile("mov    x16, #-0xe\n"
                  "svc    #0x80\n"
                  "ret");
-#elif __x86_64__
+    #elif __x86_64__
     asm volatile(".intel_syntax\n"
                  "mov    r10, rcx\n"
                  "mov    eax, 0x100000e\n"
                  "syscall\n"
                  "ret");
-#endif
+    #endif
 }
+#endif
 
 static inline void copy_mem(void *destination, const void *source, size_t len) {
     unsigned char *dst = (unsigned char *)destination;
@@ -65,13 +69,13 @@ int write_mem(void *destination, const void *source, size_t len) {
     ARG_CHECK(len != 0);
     mach_port_t task = mach_task_self();
     mach_vm_address_t dst = (mach_vm_address_t)destination;
-    int kr = mach_vm_protect_trap(task, dst, len, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    int kr = mach_vm_protect(task, dst, len, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
     if (kr != 0) {
         LOG_ERROR("mach_vm_protect failed for address %p: %s", destination, mach_error_string(kr));
         return kr;
     }
     copy_mem(destination, source, len);
-    mach_vm_protect_trap(task, dst, len, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+    mach_vm_protect(task, dst, len, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
     // might fail when editing __DATA, but not a big deal
     return 0;
 }
